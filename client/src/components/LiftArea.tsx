a61dbc66450fb70458f68e4e0d398c87f857cc09
Fauzan Rhamzy <fauzan.rhamzy01@gmail.com>
2026-01-21 05:48:03 +0700

    Update LiftArea.tsx

--- client/src/components/LiftArea.tsx
+++ client/src/components/LiftArea.tsx
@@ -1,15 +1,13 @@
 function LiftArea() {
-  // Ukuran viewBox bisa disesuaikan, di sini saya pakai 100x120
-  // agar sedikit lebih besar/beda dari slot parkir biasa
   const width = 350;
   const height = 182;
   return (
     <svg width={width} height={height} viewBox="0 0 ${width} ${height}">
       <rect width={width} height={height} fill="#9CA3AF" />
-      {/* Teks LIFT */}
+      {/* teks lift */}
       <text
         x={width / 2}
-        y={height / 2} // Posisi vertikal (tengah kotak putih)
+        y={height / 2} // posisi vertikal (tengah kotak putih)
         textAnchor="middle"
       >
         Area Lift
